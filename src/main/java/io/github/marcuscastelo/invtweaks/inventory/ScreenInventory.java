250414e70bfea64593ecbc3ac9f0f6f772441c07
marcuscastelo <marcuscastelo@usp.br>
2022-08-29 11:21:35 -0300

    Restart project from template 1.19.2

--- src/main/java/io/github/marcuscastelo/invtweaks/inventory/ScreenInventory.java
+++
@@ -1,9 +0,0 @@
-package io.github.marcuscastelo.invtweaks.inventory;
-
-import net.minecraft.screen.ScreenHandler;
-
-public record ScreenInventory(ScreenHandler screenHandler, int start, int end) {
-    public int getSize() {
-        return end - start + 1;
-    }
-}
