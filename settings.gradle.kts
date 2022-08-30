1a80791599c276e673fbaf3696a907efc1414f99
marcuscastelo <marcuscastelo@usp.br>
2022-08-29 22:00:58 -0300

    Load kotlin template

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "marucs-invtweaks"
+pluginManagement {
+    repositories {
+        jcenter()
+        maven("https://maven.fabricmc.net/") {
+            name = "Fabric"
+        }
+        gradlePluginPortal()
+    }
+
+    plugins {
+        id("fabric-loom") version "0.11-SNAPSHOT"
+        id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    }
+
+}
