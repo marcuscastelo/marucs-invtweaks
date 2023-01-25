2b909a96286d0f0639af7f2fc59b4a745c492d9c
marcuscastelo <marcuscastelo@usp.br>
2023-01-25 00:42:31 -0300

    Delete everything

--- src/main/java/io/github/marcuscastelo/invtweaks/OperationExecutor.java
+++
@@ -1,6 +0,0 @@
-package io.github.marcuscastelo.invtweaks;
-
-@FunctionalInterface
-public interface OperationExecutor {
-    void execute(InvTweaksOperationInfo operationInfo);
-}
--- src/main/kotlin/io/github/marcuscastelo/invtweaks/input/IInputProvider.kt
+++
@@ -1,6 +0,0 @@
-package io.github.marcuscastelo.invtweaks.input
-
-interface IInputProvider {
-    fun isDropOperation(): Boolean
-    fun getPressedButton(): Int
-}
